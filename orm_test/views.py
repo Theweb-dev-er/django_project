de8f012d3549129c328e08cca97b43b7d610a8ab
chetan_patil <chetan.wdev@gmail.com>
2023-05-27 13:48:48 +0530

    test_pull

--- orm_test/views.py
+++ orm_test/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('welcome')
+    return HttpResponse('welcome gfgfg')
     
 
 # Create your views here.
