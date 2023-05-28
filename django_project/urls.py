a1b2bdb7ce9805a2f021c74f3dad419bb36f69bb
chetan_patil <chetan.wdev@gmail.com>
2023-05-28 15:32:17 +0530

    query builder structure

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('orm_test.urls'))
+    path('',include('orm_test.urls')),
+    path('query-builder', include('query_builder.urls')),
     
 ]
